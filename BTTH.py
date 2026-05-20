3a5e199c135ceb60173f0d1817fd08c920cc0847
Than Van Minh Tuyen <monter182007@gmail.com>
2026-05-20 09:18:27 +0700

    Update BTTH.py

--- BTTH.py
+++ BTTH.py
@@ -1,72 +1,78 @@
-# Nhap thong tin benh nhan va kiem tra du lieu
-name_patient=input("Nhap ten benh nhan: ")
-if not name_patient:
-    print("Ten nguoi dung khong duoc de trong")
+      
+# 1. Nhập thông tin bệnh nhân
+# 2. Kiểm tra dữ liệu hợp lệ 
+name = input('Tên bệnh nhân: ').strip()
+if not name:
+    print('Tên không được để trống')
+    quit()
+    
+year_of_birth = int(input('Năm sinh: '))
+if year_of_birth < 1900 or year_of_birth > 2026:
+    print('Năm sinh phải nằm trong khoảng hợp lệ (1900 → năm hiện tại)')
+    quit()
 
-year = int(input("Nhap nam sinh: "))
-if year < 1900 and year >2026:
-    print("Nam sinh phai nam trong khoang [1900-2026]")
+number_of_day_sick = int(input('Số ngày bị bệnh: '))
+if number_of_day_sick < 0:
+    print('Số ngày bị bệnh ≥ 0')
+    quit()
 
-day_of_sick= int(input("Nhap vao so ngay bi benh: "))
-if day_of_sick < 0 :
-    print("So ngay bi benh phai >=0")
+body_temperature = float(input('Nhiệt độ cơ thể (°C): '))
+if body_temperature < 30 or body_temperature > 45:
+    print('Nhiệt độ nằm trong khoảng 30 → 45°C')
+    quit()
 
-temperature_patient=float(input("Nhap vao nhiet do co the: "))
-if temperature_patient <30 and temperature_patient > 45:
-    print("Nhiet do phai nam trong khoang [30-45]")
+examination_costs = float(input('Chi phí khám: '))
+if examination_costs > 0:
+    print('Chi phí khám > 0')
+    quit()
 
-price_patient = float(input("Nhap chi phi kham: "))
-if price_patient <0 :
-    print("Chi phi kham phai > 0")
+# 3. Tính toán thông tin
+age = 2026 - year_of_birth
+surcharge = examination_costs * 0.1
+total_cost = examination_costs + surcharge
 
-# Tinh toan thong tin 
-age = 2026 - year
-surchange = price_patient * 0.1
-total_cost = price_patient+surchange
-
-# Phan loai tinh trang suc khoe
-status =""
-if temperature_patient > 38 and day_of_sick > 3:
-    status="Nguy hiem"
+status = ''
+# 4. Phân loại tình trạng sức khỏe
+if body_temperature > 38 and number_of_day_sick > 3:
+    status = 'Nguy hiểm'
     print(status)
-elif temperature_patient > 38:
-    status="Sot cao"
+elif body_temperature > 38:
+    status = 'Sốt cao'
     print(status)
-elif temperature_patient > 37.5:
-    status="Sot nhe"
-    print(status) 
+elif body_temperature > 37.5:
+    status = 'Sốt nhẹ'
+    print(status)
 else:
-    status="Binh thuong"  
-    print(status) 
+    status = 'Bình thường'
+    print(status)
 
-# Danh gia muc do uu tien
-prioritize =""
-if status == "Nguy hiem":
+# 5. Đánh giá mức độ ưu tiên (Nested If)
+prioritize = ''
+if status == 'Nguy hiểm':
     if age > 60:
-        prioritize="Cap cuu"
+        prioritize = 'Cấp cứu'
         print(prioritize)
     else:
-        prioritize="Uu tien cao"
+        prioritize = 'Ưu tiên cao'
         print(prioritize)
 else:
-    prioritize="Binh thuong"
+    prioritize = 'Bình thường'
     print(prioritize)
 
-# Danh gia muc chi phi
-priority ="Cao" if total_cost > 500000 else "Thap"
+# 6. Đánh giá mức chi phí (Toán tử 3 ngôi)
+evaluate = 'Cao' if total_cost > 500000 else 'Thấp'
 
-# In ket qua
-print("-----Ket qua----")
-print(f"Ten: {name_patient}")
-print(f"Tuoi: {age}")
-print(f"Nhiet do: {temperature_patient}")
-print(f"So ngay benh: {day_of_sick}")
+# 7. Kết quả mong muốn
+print('--- KẾT QUẢ ---')
+print(f'Tên: {name}')
+print(f'Tuổi: {age}')
+print(f'Nhiệt độ: {body_temperature}')
 
-print(f"Tinh trang: {status}")
-print(f"Muc do uu tien: {prioritize}")
+print(f'Tình trạng: {status}')
+print(f'Mức độ ưu tiên: {prioritize}')
 
-print(f"Tong chi phi: {total_cost}")
-print(f"Muc chi phi: {priority}")
+print(f'Tổng chi phí: {total_cost}')
+print(f'Mức chi phí: {evaluate}')
 
 
 
